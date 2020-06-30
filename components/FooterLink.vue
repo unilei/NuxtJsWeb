478f128a9c7a23f2d4a641477a94b0b6d3f2ffa2
xuliulei <xuliulei666@163.com>
2020-06-30 10:06:14 +0800

    友情链接修改

--- components/FooterLink.vue
+++ components/FooterLink.vue
@@ -4,16 +4,11 @@
     <ul>
       <!--          <li><a href="#">友情链接</a></li>-->
       <li><a href="javascript:;">友情链接</a></li>
-
-<!--      <li><a href="https://www.zuqiuvs.com/" target="_blank">足球比分网</a></li>-->
       <li><a href="https://www.evfootball.com" target="_blank">一维足球</a></li>
       <li><a href="https://www.soulele.com" target="_blank">搜乐足球</a></li>
       <li><a href="http://www.jinq.com" target="_blank">进球网</a></li>
       <li><a href="http://www.tc108.com" target="_blank">畅说</a></li>
       <li><a href="http://www.cctv-3.net" target="_blank">3直播吧</a></li>
-      <li><a href="http://www.qieerxi.net" target="_blank"> 切尔西吧</a></li>
-      <li><a href="http://www.qiuyouba.com" target="_blank">足球预测网</a></li>
-
     </ul>
   </div>
 
