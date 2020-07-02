c992e5ce03796b3cddf3e9c07d8075b1c9152a68
xuliulei <xuliulei666@163.com>
2020-07-02 14:01:27 +0800

    根据hostname显示不同的代码

--- components/FooterBottom.vue
+++ components/FooterBottom.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="footer-d-container">
-    <div class="footer-d-d">
+    <div class="footer-d-d" v-if="hostname !== 'www.51体育.net' && hostname !== '51体育.net'">
                 <span>枢秘（杭州）信息科技有限公司
                     <a rel="nofollow" href="http://beian.miit.gov.cn/" target="_blank">浙ICP备18019159号-2</a>
                     <a rel="nofollow" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010402004047"
                        target="_blank">浙公网安备浙公网安备 33010402004047号</a>
+                    举报电话：+8613482207807   举报邮箱：customer@nsnni.com   Copyright © 全民体育</span>
+    </div>
+    <div class="footer-d-d" v-if="hostname === 'www.51体育.net' || hostname === '51体育.net'">
+                <span>上海辣森网络科技有限公司
+                    <a rel="nofollow" href="http://beian.miit.gov.cn/" target="_blank">沪ICP备18038979号-4</a>
+                    <a rel="nofollow" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=31010702006081"
+                       target="_blank">沪公安备案号:31010702006081</a>
                     举报电话：+8613482207807   举报邮箱：customer@nsnni.com   Copyright © 全民体育</span>
     </div>
   </div>
@@ -24,10 +31,13 @@
       return {
         os: '',
         isShowLink:false,
+        hostname:''
       }
     },
     mounted () {
       this.os = GetOs()
+      this.hostname = document.location.hostname;
+      console.log(this.hostname)
     }
   }
 </script>
